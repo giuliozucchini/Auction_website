13087f1cf5fb5370ddd49a31741b05a4bc4f15c3
Giulio Zucchini <giulio.zucchini@gmail.com>
2015-07-23 10:47:13 +0200

    Auction website

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+	root "products#index"
+
+  resources :users, only: [:index, :show, :new, :create, :destroy] do 
+  	resources :products, only: [:index, :show, :new, :create, :destroy]
+  end
+  resources :products, only: [:index, :create, :new, :show] do
+	  resources :bids, only: [:create]
+  end
+
+
+end
